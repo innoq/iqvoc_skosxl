d13684d15ded05935d1356d50925888d145cb64e
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-04-06 15:17:32 +0200

    Added Iqvoc configuration to skosxl standalone application

---
+++ config/engine.rb
@@ -0,0 +1,18 @@
+require 'rails'
+
+module Iqvoc
+  module SKOSXL
+
+    class Engine < Rails::Engine
+      
+      paths.lib.tasks  << "lib/engine_tasks"
+
+      def self.load_seed
+        seed_file = Iqvoc::SKOSXL::Engine.find_root_with_flag("db").join('db/seeds.rb')
+        load(seed_file) if File.exist?(seed_file)
+      end
+
+    end
+    
+  end
+end
--- lib/iqvoc_skosxl.rb
+++ lib/iqvoc_skosxl.rb
@@ -1,3 +1,19 @@
-module IqvocSkosxl
-  require 'iqvoc/skosxl/engine'
+module IqvocSKOSXL
+  require File.join(File.dirname(__FILE__), '../config/engine') unless Iqvoc.const_defined?(:SKOSXL) && Iqvoc::SKOSXL.const_defined?(:Application)
+
+  ActiveSupport.on_load(:after_iqvoc_config) do
+    require('iqvoc')
+ 
+    Iqvoc.rdf_namespaces[:skosxl] = "http://www.w3.org/2008/05/skos-xl#" unless Iqvoc.rdf_namespaces[:skosxl]
+
+    Iqvoc.default_rdf_namespace_helper_methods << :iqvoc_skosxl_default_rdf_namespaces
+
+    Iqvoc::Concept.pref_labeling_class_name = 'Labeling::SKOSXL::PrefLabel'
+    Iqvoc::Concept.further_labeling_class_names = {
+      'Labeling::SKOSXL::AltLabel' => [ :de, :en ]
+    }
+
+    #   TODO     Iqvoc.searchable_class_names = Iqvoc::Concept.labeling_class_names.keys + Iqvoc::Concept.note_class_names
+  end
+
 end

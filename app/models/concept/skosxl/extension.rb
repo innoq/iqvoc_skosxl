29c74dafb5e6af3410f7b8b147dda58f58a7e44a
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-04-08 09:45:41 +0200

    Added Concept::Base extension with SKOSXL specific concept extensions

---
+++ app/models/concept/skosxl/extension.rb
@@ -0,0 +1,45 @@
+module Concept
+  module SKOSXL
+    module Extension
+
+      extend ActiveSupport::Concern
+
+      included do
+
+        after_save do |concept|
+          # Labelings
+          (@labelings_by_id ||= {}).each do |labeling_relation_name, origin_mappings|
+            # Remove all associated labelings of the given type
+            concept.send(labeling_relation_name).destroy_all
+
+            # (Re)create labelings reflecting a widget's parameters
+            origin_mappings.each do |language, new_origins|
+              new_origins = new_origins.split(/[,\n]/).map(&:squish)
+
+              # Iterate over all labels to be added and create them
+              Iqvoc::XLLabel.base_class.by_origin(new_origins).each do |l|
+                concept.send(labeling_class_name.to_relation_name).create!(:target => l)
+              end
+            end
+          end
+
+        end
+
+      end
+
+      module InstanceMethods
+
+        def labelings_by_id=(hash)
+          @labelings_by_id = hash
+        end
+
+        def labelings_by_id(relation_name, language)
+          (@labelings_by_id && @labelings_by_id[relation_name] && @labelings_by_id[relation_name][language]) ||
+            self.send(relation_name).by_label_language(language).map{ |l| l.target.origin }.join(",")
+        end
+
+      end
+      
+    end
+  end
+end
--- lib/iqvoc_skosxl.rb
+++ lib/iqvoc_skosxl.rb
@@ -1,12 +1,13 @@
 module IqvocSKOSXL
   require File.join(File.dirname(__FILE__), '../config/engine') unless Iqvoc.const_defined?(:SKOSXL) && Iqvoc::SKOSXL.const_defined?(:Application)
-
   ActiveSupport.on_load(:after_iqvoc_config) do
     require('iqvoc')
- 
+
     Iqvoc.rdf_namespaces[:skosxl] = "http://www.w3.org/2008/05/skos-xl#" unless Iqvoc.rdf_namespaces[:skosxl]
 
     Iqvoc.default_rdf_namespace_helper_methods << :iqvoc_skosxl_default_rdf_namespaces
+
+    Iqvoc::Concept.include_module_names << "Concept::SKOSXL::Extension"
 
     Iqvoc::Concept.pref_labeling_class_name = 'Labeling::SKOSXL::PrefLabel'
     Iqvoc::Concept.further_labeling_class_names = {

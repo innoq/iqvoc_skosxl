04a567380311b504f09d62a0ec5d1b8440483cf1
Robert Glaser <mail@robert-glaser.de>
2012-01-10 14:15:29 +0100

    debug_rjs has been removed in Rails 3.1.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ if Iqvoc::SKOSXL.const_defined?(:Application)
 
     # Show full error reports and disable caching
     config.consider_all_requests_local       = true
-    config.action_view.debug_rjs             = true
     config.action_controller.perform_caching = false
 
     # Don't care if the mailer can't send

72187b6189dcb04ff594918f8ef019bb0f1cc4e0
FND <fnd@fnd-oda.(none)>
2012-01-11 07:19:12 +0100

    removed obsolete seed initialization
    no longer required with Rails 3.1

--- config/engine.rb
+++ config/engine.rb
@@ -7,11 +7,6 @@ module Iqvoc
 
       paths.lib.tasks  << "lib/engine_tasks"
 
-      def self.load_seed
-        seed_file = Iqvoc::SKOSXL::Engine.find_root_with_flag("db").join('db/seeds.rb')
-        load(seed_file) if File.exist?(seed_file)
-      end
-
     end
 
   end

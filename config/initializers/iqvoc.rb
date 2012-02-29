65dab9967d603a26a10749c45c0bd8001468365e
FND <fnd@fnd-oda.(none)>
2012-02-29 08:05:13 +0100

    silenced warning for non-XL label deactivation

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,4 +1,6 @@
-Iqvoc::Label = nil
+silence_warnings do
+  Iqvoc::Label = nil
+end
 
 Iqvoc.searchable_class_names = ['Labeling::SKOSXL::Base'] +
     Iqvoc::Concept.labeling_class_names.keys + Iqvoc::Concept.note_class_names

51453acd41c7772020c57085cd6545545191ead4
Robert Glaser <mail@robert-glaser.de>
2012-11-20 10:33:17 +0100

    simplified engine migrations

--- config/engine.rb
+++ config/engine.rb
@@ -4,9 +4,11 @@ module Iqvoc
   module SKOSXL
 
     class Engine < Rails::Engine
-
       paths["lib/tasks"] << "lib/engine_tasks"
 
+      initializer "iqvoc_skosxl.load_migrations" do |app|
+        app.config.paths['db/migrate'] += Iqvoc::SKOSXL::Engine.paths['db/migrate'].existent
+      end
     end
 
   end

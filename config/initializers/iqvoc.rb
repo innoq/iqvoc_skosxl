99968f99c5daf01694a2a04976bd069533adf974
FND <fnd@localhost.localdomain>
2013-04-10 15:17:36 +0200

    ensured that deactivated non-XL namespace remains accessible
    erasing information is generally bad - this particular change was
    prompted by an iQvoc engine wanting to use SKOS and SKOS-XL in parallel
    (for testing, to ensure it works across many configurations)
    
    heads-up @mrreynolds @tillsc

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,5 +1,29 @@
+class DisabledNamespace
+
+  class << self
+    attr_accessor :obsolete
+  end
+
+  def self.method_missing(meth, *args, &block)
+    error
+  end
+
+  def self.const_missing(name)
+    error
+  end
+
+  def self.error
+    warn <<-EOS.strip.gsub(/\s+/, " ")
+      this namespace has been disabled - the original functionality remains
+      available via `obsolete`
+    EOS
+  end
+
+end
+
+DisabledNamespace.obsolete = Iqvoc::Label
 silence_warnings do
-  Iqvoc::Label = nil
+  Iqvoc::Label = DisabledNamespace
 end
 
 Iqvoc.searchable_class_names = ['Labeling::SKOSXL::Base'] +

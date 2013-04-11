f5822b810b1d800a7711eeca1a092b873b47f504
FND <fnd@localhost.localdomain>
2013-04-11 10:12:12 +0200

    Revert "ensured that deactivated non-XL namespace remains accessible"
    this extension does *way* more than just replacing `Iqvoc::Label` -
    selectively (de)activating it turns out to be impractical
    
    This reverts commits 99968f99c5daf01694a2a04976bd069533adf974 and
    11e03916afd7b67b34602ef3423263546e700b8d.

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,30 +1,5 @@
-class DisabledNamespace
-
-  attr_reader :obsolete
-
-  def initialize(obsolete_namespace)
-    @obsolete = obsolete_namespace
-  end
-
-  def method_missing(meth, *args, &block)
-    error
-  end
-
-  def const_missing(name)
-    error
-  end
-
-  def error
-    raise RuntimeError, <<-EOS.strip.gsub(/\s+/, " ")
-      this namespace has been disabled - the original functionality remains
-      available via `obsolete`
-    EOS
-  end
-
-end
-
 silence_warnings do
-  Iqvoc::Label = DisabledNamespace.new(Iqvoc::Label)
+  Iqvoc::Label = nil
 end
 
 Iqvoc.searchable_class_names = ['Labeling::SKOSXL::Base'] +

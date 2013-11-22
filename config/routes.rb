7256bcf9ee51515578f5d14cbfe75bf401e7b449
Robert Glaser <mail@robert-glaser.de>
2013-11-22 12:33:01 +0100

    adopt rails 4 defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Iqvoc.localized_routes << lambda do |routing|
   routing.resources :labels
 
-  routing.match "labels/versions/:origin/branch(.:format)"      => "labels/versions#branch",    :as => "label_versions_branch"
-  routing.match "labels/versions/:origin/merge(.:format)"       => "labels/versions#merge",     :as => "label_versions_merge"
-  routing.match "labels/versions/:origin/lock(.:format)"        => "labels/versions#lock",      :as => "label_versions_lock"
-  routing.match "labels/versions/:origin/unlock(.:format)"      => "labels/versions#unlock",    :as => "label_versions_unlock"
-  routing.match "labels/versions/:origin/to_review(.:format)"   => "labels/versions#to_review", :as => "label_versions_to_review"
-  routing.match "labels/versions/:origin/consistency_check(.:format)" => "labels/versions#consistency_check", :as => "label_versions_consistency_check"
-end
-
-Rails.application.routes.draw do
+  routing.post "labels/versions/:origin/branch(.:format)"      => "labels/versions#branch",    :as => "label_versions_branch"
+  routing.post "labels/versions/:origin/merge(.:format)"       => "labels/versions#merge",     :as => "label_versions_merge"
+  routing.post "labels/versions/:origin/lock(.:format)"        => "labels/versions#lock",      :as => "label_versions_lock"
+  routing.post "labels/versions/:origin/unlock(.:format)"      => "labels/versions#unlock",    :as => "label_versions_unlock"
+  routing.post "labels/versions/:origin/to_review(.:format)"   => "labels/versions#to_review", :as => "label_versions_to_review"
+  routing.get "labels/versions/:origin/consistency_check(.:format)" => "labels/versions#consistency_check", :as => "label_versions_consistency_check"
 end

d919630c1e1fd936a86dd57457ef414eb4bbd510
Robert Glaser <mail@robert-glaser.de>
2013-11-25 16:49:14 +0100

    fix concat of engine paths

--- config/engine.rb
+++ config/engine.rb
@@ -7,7 +7,7 @@ module Iqvoc
       paths["lib/tasks"] << "lib/engine_tasks"
 
       initializer "iqvoc_skosxl.load_migrations" do |app|
-        app.config.paths['db/migrate'] << Iqvoc::SKOSXL::Engine.paths['db/migrate'].existent
+        app.config.paths['db/migrate'].concat(Iqvoc::SKOSXL::Engine.paths['db/migrate'].existent)
       end
     end
 

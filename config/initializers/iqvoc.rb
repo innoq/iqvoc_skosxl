6fb9a17bef3525ff9be84d04b7e7b7b09a9a6587
Robert Glaser <mail@robert-glaser.de>
2014-01-13 16:13:05 +0100

    adopt to new config style for searchable_class_names

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -2,5 +2,6 @@ silence_warnings do
   Iqvoc::Label = nil
 end
 
-Iqvoc.searchable_class_names = ['Labeling::SKOSXL::Base'] +
-    Iqvoc::Concept.labeling_class_names.keys + Iqvoc::Concept.note_class_names
+Iqvoc.searchable_class_names = { 'Labeling::SKOSXL::Base' => 'labels',
+  'Labeling::SKOSXL::PrefLabel' => 'pref_labels',
+  'Labeling::SKOSXL::AltLabel' => 'alt_labels' }

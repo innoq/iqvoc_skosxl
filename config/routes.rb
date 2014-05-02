374bacc8659a5254e6289d48e44360f4717374b3
Robert Glaser <mail@robert-glaser.de>
2014-05-02 16:39:54 +0200

    adopt Iqvoc.routing_constraint [ci skip]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
-Iqvoc.localized_routes << lambda do |routing|
-  routing.resources :labels
+Rails.application.routes.draw do
+  scope ':lang', :constraints => Iqvoc.routing_constraint do
+    resources :labels
 
-  routing.post "labels/versions/:origin/branch(.:format)"      => "labels/versions#branch",    :as => "label_versions_branch"
-  routing.post "labels/versions/:origin/merge(.:format)"       => "labels/versions#merge",     :as => "label_versions_merge"
-  routing.post "labels/versions/:origin/lock(.:format)"        => "labels/versions#lock",      :as => "label_versions_lock"
-  routing.post "labels/versions/:origin/unlock(.:format)"      => "labels/versions#unlock",    :as => "label_versions_unlock"
-  routing.post "labels/versions/:origin/to_review(.:format)"   => "labels/versions#to_review", :as => "label_versions_to_review"
-  routing.get "labels/versions/:origin/consistency_check(.:format)" => "labels/versions#consistency_check", :as => "label_versions_consistency_check"
+    post "labels/versions/:origin/branch" => "labels/versions#branch",
+        :as => "label_versions_branch"
+    post "labels/versions/:origin/merge" => "labels/versions#merge",
+        :as => "label_versions_merge"
+    post "labels/versions/:origin/lock" => "labels/versions#lock",
+        :as => "label_versions_lock"
+    post "labels/versions/:origin/unlock" => "labels/versions#unlock",
+        :as => "label_versions_unlock"
+    post "labels/versions/:origin/to_review" => "labels/versions#to_review",
+        :as => "label_versions_to_review"
+    get "labels/versions/:origin/consistency_check" => "labels/versions#consistency_check",
+        :as => "label_versions_consistency_check"
+  end
 end

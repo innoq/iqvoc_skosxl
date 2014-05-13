d4fe1c8cec1a4e4d38283feac48cd10979239c3b
Robert Glaser <mail@robert-glaser.de>
2014-05-13 18:09:53 +0200

    make label relations importable

--- app/models/label/relation/skosxl/base.rb
+++ app/models/label/relation/skosxl/base.rb
@@ -2,6 +2,10 @@ class Label::Relation::SKOSXL::Base < Label::Relation::Base
 
   self.rdf_namespace = 'skosxl'
 
+  def self.build_from_rdf(subject, predicate, object)
+    create(domain: subject, range: object)
+  end
+
   def build_rdf(document, subject)
     pred = self.class == Label::Relation::SKOSXL::Base ? :labelRelation : self.rdf_predicate
     raise "Match::SKOS::Base#build_rdf: Class #{self.class.name} needs to define self.rdf_namespace and self.rdf_predicate." unless pred
--- test/models/skos_importer_test.rb
+++ test/models/skos_importer_test.rb
@@ -6,11 +6,23 @@ require 'iqvoc/skos_importer'
 class SkosImporterTest < ActiveSupport::TestCase
   TEST_DATA = File.open(File.expand_path("../../fixtures/hobbies.nt", __FILE__))
 
+  setup do
+    Iqvoc.rdf_namespaces['test'] = 'http://foo.bar/xl#'
+
+    class TestRelation < Label::Relation::SKOSXL::Base
+      self.rdf_namespace = 'test'
+      self.rdf_predicate = 'test-relation'
+    end
+
+    @importer = Iqvoc::SkosImporter.new(TEST_DATA, "http://hobbies.com#")
+    @importer.second_level_object_classes << TestRelation
+  end
+
   test "imports xl labels with relations" do
     assert_equal 0, Concept::SKOS::Base.count
     assert_equal 0, Label::SKOSXL::Base.count
 
-    Iqvoc::SkosImporter.new(TEST_DATA, "http://hobbies.com#").run
+    @importer.run
 
     assert_equal 1, Labeling::SKOSXL::PrefLabel.count
     assert_equal 1, Labeling::SKOSXL::AltLabel.count
@@ -37,5 +49,7 @@ class SkosImporterTest < ActiveSupport::TestCase
     assert_equal 1, alt_label.target.notes_for_class(Note::SKOS::Definition).count
     assert_equal "en", alt_label.target.notes_for_class(Note::SKOS::Definition).first.language
     assert_equal "Yadda yadda", alt_label.target.notes_for_class(Note::SKOS::Definition).first.value
+    assert_equal 1, alt_label.target.relations_for_class(TestRelation).count
+    assert_equal pref_label.target, alt_label.target.relations_for_class(TestRelation).first.range
   end
 end

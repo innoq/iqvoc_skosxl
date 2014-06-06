2e970ecc2e5d9f53e8db03c371a3846a73ebecff
Marc Jansing <marc.jansing@innoq.com>
2014-06-06 11:45:39 +0200

    Load test environment only if not loaded

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+
+# Load rails environment if not loaded
+unless defined?(Iqvoc) && Iqvoc::SKOSXL.const_defined?(:Engine)
+  require File.expand_path('../../config/environment', __FILE__)
+end
+
 require 'rails/test_help'

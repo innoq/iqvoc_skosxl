c5d5c1d54c555caf56ff42ae26a344da96fc9b7a
Marc Jansing <marc.jansing@innoq.com>
2014-06-06 11:45:04 +0200

    Add label creation test

---
+++ test/integration/label_creation_test.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+
+# Copyright 2011-2014 innoQ Deutschland GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require File.join(File.expand_path(File.dirname(__FILE__)), '../integration_test_helper')
+
+class LabelCreationTest < ActionDispatch::IntegrationTest
+  setup do
+    login 'administrator'
+  end
+
+  test "label creation" do
+    visit new_label_path(lang: 'de', language: 'de')
+    assert page.has_content? 'Neues Label'
+    fill_in 'Vorlageform', with: 'Testlabel'
+    click_link_or_button 'Speichern'
+    assert page.has_content? 'Das Label wurde erstellt.'
+    click_link_or_button 'Konsistenz prüfen'
+    assert page.has_content? 'Instanz ist konsistent.'
+  end
+end

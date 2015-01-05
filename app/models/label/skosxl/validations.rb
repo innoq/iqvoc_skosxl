ab9b7034cd63d40804cf7f493b2879bcac81c4da
Robert Glaser <mail@robert-glaser.de>
2015-01-05 16:30:06 +0100

    tweak error messaging

--- app/models/label/skosxl/validations.rb
+++ app/models/label/skosxl/validations.rb
@@ -14,14 +14,14 @@ module Label
 
       def origin_has_to_be_escaped
         unless Iqvoc::Origin.new(origin).valid?
-          errors.add :origin, I18n.t('txt.models.label.origin_invalid')
+          errors.add :base, I18n.t('txt.models.label.origin_invalid')
         end
       end
 
       def value_must_be_given
         if validatable_for_publishing?
           if value.blank?
-            errors.add :origin, I18n.t('txt.models.label.value_error')
+            errors.add :base, I18n.t('txt.models.label.value_error')
           end
         end
       end

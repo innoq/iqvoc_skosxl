b7f0d182d205a92206f92eb97ebff08e55ca622a
Robert Glaser <mail@robert-glaser.de>
2015-01-06 10:22:54 +0100

    fix tests

--- test/models/label_test.rb
+++ test/models/label_test.rb
@@ -18,17 +18,18 @@ class LabelTest < ActiveSupport::TestCase
     @current_label = Iqvoc::XLLabel.base_class.create(
       language: 'en', value: 'Forest', published_at: 3.days.ago)
     duplicate = Iqvoc::XLLabel.base_class.new(
-      language: 'en', value: 'Forest', published_at: 3.days.ago)
-    refute duplicate.save
+      language: 'en', value: 'Forest')
+    assert duplicate.save
+    refute duplicate.publishable?
 
     duplicate.value = 'Forest2'
-    assert duplicate.save
+    assert duplicate.publishable?
   end
 
   test 'label origin generation' do
     l1 = Iqvoc::XLLabel.base_class.create(
       language: 'en', value: 'Forest', published_at: 3.days.ago)
-    assert_match /_[0-9a-z]{16}/, l1.origin
+    assert_match /_[0-9a-z]{8}/, l1.origin
 
     l2 = Iqvoc::XLLabel.base_class.create(
       language: 'en', value: 'Forest', origin: 'forest', published_at: 3.days.ago)
@@ -41,7 +42,7 @@ class LabelTest < ActiveSupport::TestCase
     l2 = Iqvoc::XLLabel.base_class.new(
       language: 'en', value: 'Forest', published_at: 3.days.ago)
     assert l2.save
-    assert_match /_[0-9a-z]{16}/, l2.origin
+    assert_match /_[0-9a-z]{8}/, l2.origin
   end
 
   test 'should validate origin for escaping' do

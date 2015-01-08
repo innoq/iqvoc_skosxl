4ca38892e9897fb6a941a3697a352cc8fd59d00d
Robert Glaser <mail@robert-glaser.de>
2015-01-08 16:56:17 +0100

    fix class name

--- db/migrate/20150108154312_add_fk_contraints_iqvoc_skosxl.rb
+++ db/migrate/20150108154312_add_fk_contraints_iqvoc_skosxl.rb
@@ -1,7 +1,7 @@
-class AddFkContraints < ActiveRecord::Migration
+class AddFkContraintsIqvocSkosxl < ActiveRecord::Migration
   def change
     add_foreign_key :labels, :labels, column: 'published_version_id', on_update: :cascade, on_delete: :cascade
     add_foreign_key :label_relations, :labels, column: 'domain_id', on_update: :cascade, on_delete: :cascade
-    add_foreign_key :label_relations, :labels, column: 'range_id', on_update: :cascade, on_delete: :cascade 
+    add_foreign_key :label_relations, :labels, column: 'range_id', on_update: :cascade, on_delete: :cascade
   end
 end

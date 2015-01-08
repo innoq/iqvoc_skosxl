0be663d217013794ef2069a9007647df9a92f914
Robert Glaser <mail@robert-glaser.de>
2015-01-08 16:48:12 +0100

    add foreign key constraints

---
+++ db/migrate/20150108154312_add_fk_contraints.rb
@@ -0,0 +1,7 @@
+class AddFkContraints < ActiveRecord::Migration
+  def change
+    add_foreign_key :labels, :labels, column: 'published_version_id', on_update: :cascade, on_delete: :cascade
+    add_foreign_key :label_relations, :labels, column: 'domain_id', on_update: :cascade, on_delete: :cascade
+    add_foreign_key :label_relations, :labels, column: 'range_id', on_update: :cascade, on_delete: :cascade 
+  end
+end

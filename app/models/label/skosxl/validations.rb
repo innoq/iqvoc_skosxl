176baba1465ebd0c17ca18591ca314e1e358a930
Marc Jansing <marc.jansing@innoq.com>
2015-04-14 16:19:03 +0200

    add referenced concepts pref label validation

--- app/models/label/skosxl/validations.rb
+++ app/models/label/skosxl/validations.rb
@@ -8,14 +8,29 @@ module Label
         validates :origin, uniqueness: { scope: :rev }
         validate :origin_has_to_be_escaped
 
+        validate :referenced_published_concepts_has_to_be_valid, if: :validatable_for_publishing?
         validates :value, uniqueness: { scope: [:language, :rev] },
-                          if: :validatable_for_publishing?
+          if: :validatable_for_publishing?
         validates :value, presence: true, if: :validatable_for_publishing?
       end
 
       def origin_has_to_be_escaped
         unless Origin.new(origin).valid?
           errors.add :base, I18n.t('txt.models.label.origin_invalid')
+        end
+      end
+
+      def referenced_published_concepts_has_to_be_valid
+        published_label = self.published_version
+
+        # check published concepts which use the current label as a pref label
+        concepts.published.each do |concept|
+          # pref labels without the current published one
+          labels = concept.pref_labels.reject{ |l| l == published_label }
+
+          unless labels.map(&:language).map(&:to_s).include?(Iqvoc::Concept.pref_labeling_languages.first.to_s)
+            errors.add :base, I18n.t('txt.models.label.referenced_concepts_invalid')
+          end
         end
       end
 

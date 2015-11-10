03ee51c952e222f90b247710ff36613fdefed249
Marc Jansing <marc.jansing@innoq.com>
2015-11-10 09:05:08 +0100

    fix foreign key contraint removal to fix travis build

--- db/migrate/20150414083600_remove_fk_constraints_iqvoc_skosxl.rb
+++ db/migrate/20150414083600_remove_fk_constraints_iqvoc_skosxl.rb
@@ -1,7 +1,5 @@
 class RemoveFkConstraintsIqvocSkosxl < ActiveRecord::Migration
   def change
-    if foreign_keys(:labels).detect {|fk| fk.column == 'published_version_id' }
-      remove_foreign_key :labels, column: 'published_version_id'
-    end
+    remove_foreign_key :labels, column: 'published_version_id'
   end
 end

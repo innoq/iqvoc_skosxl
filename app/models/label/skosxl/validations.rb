4976a6d765a05df12635775b4a9554ec4f2ac1df
Marc Jansing <marc.jansing@innoq.com>
2016-05-09 15:28:30 +0200

    fix referenced concepts validations when modifying label language

--- app/models/label/skosxl/validations.rb
+++ app/models/label/skosxl/validations.rb
@@ -8,7 +8,7 @@ module Label
         validates :origin, uniqueness: { scope: :rev }
         validate :origin_has_to_be_escaped
 
-        validate :referenced_published_concepts_has_to_be_valid, if: :validatable_for_publishing?
+        validate :pref_label_language, if: :validatable_for_publishing?
         validates :value, presence: true, if: :validatable_for_publishing?
       end
 
@@ -18,17 +18,9 @@ module Label
         end
       end
 
-      def referenced_published_concepts_has_to_be_valid
-        published_label = self.published_version
-
-        # check published concepts which use the current label as a pref label
-        concepts.published.each do |concept|
-          # pref labels without the current published one
-          labels = concept.pref_labels.reject{ |l| l == published_label }
-
-          unless labels.map(&:language).map(&:to_s).include?(::Iqvoc::Concept.pref_labeling_languages.first.to_s)
-            errors.add :base, I18n.t('txt.models.label.referenced_concepts_invalid')
-          end
+      def pref_label_language
+        if language != Iqvoc::Concept.pref_labeling_languages.first.to_s && concepts_for_labeling_class(Iqvoc::Concept::pref_labeling_class).any?
+          errors.add :base, I18n.t('txt.models.label.pref_label_language')
         end
       end
 

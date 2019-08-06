2e7754cea02757195a4f00d8b40857f1c3318aab
Marc Jansing <marc.jansing@innoq.com>
2019-08-06 17:33:16 +0200

    client side duplicate check

---
+++ app/assets/javascripts/iqvoc_skosxl/duplicate_check.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function() {
+  $('#label_value').on('change', function () {
+    $this = $(this);
+    var target = $this.data('query-url');
+    var labelValue = this.value;
+
+    queryLabels(target, labelValue).then(function(labels) {
+      resetInput($this);
+
+      if (labels.length > 0) {
+        setWarningState($this);
+        setFeedback($this, labels);
+      }
+    });
+
+  });
+
+  function queryLabels(target, value) {
+    return new Promise(function(resolve, _reject) {
+      if (value === '' && value.length < 4) {
+        resolve([])
+        return;
+      };
+
+      $.ajax({
+        url: target,
+        data: {
+          query: value
+        }
+      })
+      .done(function(response) {
+        console.log('response: ', response);
+        resolve(response);
+      })
+      .fail(function() {
+        resolve([]); // return empty result on failure
+      })
+    });
+  }
+
+  function setWarningState($valueInput) {
+    var formGroup = $valueInput.closest('.form-group');
+    $(formGroup).addClass('has-warning');
+  }
+
+  function setFeedback($valueInput, labels) {
+    var duplicates = buildLabelList($valueInput, labels);
+    var message = $valueInput.data('duplicate-message');
+    var feedback = $('<span class="help-block"></span>')
+      .text(message)
+      .append(duplicates);
+
+    $valueInput.after(feedback);
+  }
+
+  function buildLabelList($valueInput, labels) {
+    var ul = $('<ul class="list-inline"></ul>')
+    var lis =  labels.map(function(label) {
+      var uriTemplate = $valueInput.data('data-entity-uri');
+      return $('<li>').append(buildLabelLink(label, uriTemplate))
+    });
+
+    return ul.append(lis);
+  }
+
+  function buildLabelLink(label, uriTemplate) {
+    var uri = uriTemplate.replace("%7Bid%7D", label.id);
+    if (!label.published) {
+      uri = uri.concat('?published=0');
+    }
+
+    labelLink = $('<a>', {
+      text: label.name,
+      href: uri
+    });
+    return labelLink;
+  }
+
+  function resetInput($valueInput) {
+    var formGroup = $valueInput.closest('.form-group');;
+    $(formGroup).removeClass('has-warning');
+    $(formGroup).find('.help-block').remove();
+  }
+});
--- app/assets/javascripts/iqvoc_skosxl/manifest.js
+++ app/assets/javascripts/iqvoc_skosxl/manifest.js
@@ -1,1 +1,2 @@
 //= require iqvoc_skosxl/new_label_modal
+//= require iqvoc_skosxl/duplicate_check

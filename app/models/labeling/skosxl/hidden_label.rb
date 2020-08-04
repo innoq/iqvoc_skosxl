96ff22c325e67433d2fffdd7a8bdcdffa42fd38e
Marc Jansing <marc.jansing@innoq.com>
2020-08-04 15:38:54 +0200

    render hidden labels not in hidden tab anymore

--- app/models/labeling/skosxl/hidden_label.rb
+++ app/models/labeling/skosxl/hidden_label.rb
@@ -1,8 +1,4 @@
 class Labeling::SKOSXL::HiddenLabel < Labeling::SKOSXL::Base
   self.rdf_namespace = 'skosxl'
   self.rdf_predicate = 'hiddenLabel'
-
-  def self.view_section(obj)
-    'hidden'
-  end
 end

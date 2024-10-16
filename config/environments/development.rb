cf9cece7d2bdb6852162bd4bb5668760d9217810
Marc Jansing <marc.jansing@innoq.com>
2024-10-16 08:40:23 +0200

    wip: rails 7.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,6 @@ require 'iqvoc/environments/development'
 if Iqvoc::SKOSXL.const_defined?(:Application)
   Iqvoc::SKOSXL::Application.configure do
     # Settings specified here will take precedence over those in config/environment.rb
-    Iqvoc::Environments.setup_development(config)
+    Iqvoc::Environments::Development.setup_development(config)
   end
 end

43674ba9900ec6508bfc79f9de1febd4526b8e74
Marc Jansing <marc.jansing@innoq.com>
2025-02-17 16:08:25 +0100

    adapt iqvoc changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,6 @@ require 'iqvoc/environments/test'
 if Iqvoc::SKOSXL.const_defined?(:Application)
   Iqvoc::SKOSXL::Application.configure do
     # Settings specified here will take precedence over those in config/environment.rb
-    Iqvoc::Environments::Test.setup_test(config)
+    Iqvoc::Environments::Test.setup(config)
   end
 end

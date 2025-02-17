f7354f542b508c932a4625b7b3749f535394e42a
Marc Jansing <marc.jansing@innoq.com>
2025-02-17 21:17:49 +0100

    move test config into test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+# FIXME: needed in integration/label_creation_test for config.eager_load = true at rails boot
+Iqvoc::Xllabel.relation_class_names = [
+  'Label::Relation::Skosxl::Translation',
+  'Label::Relation::Skosxl::UnidirectionalRelation'
+]
+
 # Load rails environment if not loaded
 unless defined?(Iqvoc) && defined?(Iqvoc::Skosxl) && Iqvoc::Skosxl.const_defined?(:Engine)
   require File.expand_path('../../config/environment', __FILE__)

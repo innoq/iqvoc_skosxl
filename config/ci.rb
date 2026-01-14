316cf1a6e21310ca561fa29017aaf89f9203f1bb
Marc Jansing <marc.jansing@innoq.com>
2026-01-14 16:57:11 +0100

    remove importmaps audit from ci

--- config/ci.rb
+++ config/ci.rb
@@ -3,7 +3,7 @@
 CI.run do
   step "Setup", "bin/setup --skip-server"
 
-  step "Security: Importmap vulnerability audit", "bin/importmap audit"
+  # step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Tests: Rails", "bin/rails test"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 

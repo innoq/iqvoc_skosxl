e995fceb13a797399a6b319f9960941d9ea40874
Marc Jansing <marc.jansing@innoq.com>
2026-09-24 15:41:03 +0200

    fix migration

--- db/migrate/20110408123644_add_label_relations.rb
+++ db/migrate/20110408123644_add_label_relations.rb
@@ -1,22 +1,17 @@
 class AddLabelRelations < ActiveRecord::Migration[4.2]
-  def self.up
-    unless table_exists?('label_relations')
-      create_table 'label_relations', force: true do |t|
-        t.string   'type'
-        t.integer  'domain_id'
-        t.integer  'range_id'
-        t.datetime 'created_at'
-        t.datetime 'updated_at'
-      end
-
-      add_index 'label_relations', ['domain_id', 'range_id', 'type'], name: 'ix_label_rel_dom_rng_type'
-      add_index 'label_relations', ['type'], name: 'ix_label_relations_on_type'
+  def up
+    create_table :label_relations, force: true do |t|
+      t.string   :type
+      t.integer  :domain_id
+      t.integer  :range_id
+      t.timestamps
     end
+
+    add_index 'label_relations', ['domain_id', 'range_id', 'type'], name: 'ix_label_rel_dom_rng_type'
+    add_index 'label_relations', ['type'], name: 'ix_label_relations_on_type'
   end
 
-  def self.down
-    if table_exists?('label_relations')
-      drop_table('label_relations')
-    end
+  def down
+    drop_table :label_relations
   end
 end
